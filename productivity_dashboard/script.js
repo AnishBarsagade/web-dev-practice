18d0c4e01f81d440c2eb210b7041a6945fc92ab2
AnishBarsagade <anishbarsagade5@gmail.com>
2026-08-04 17:48:57 +0530

    Completed the frontend of todo-list

--- productivity_dashboard/script.js
+++ productivity_dashboard/script.js
@@ -1,8 +1,27 @@
-var allElems = document.querySelectorAll('.elem');
-console.log(allElems);
-
-allElems.forEach(function (elem) {
-  elem.addEventListener('click', function () {
-    console.log('Hello');
+function openFeatures() {
+  let allElems = document.querySelectorAll('.elem');
+  let allFullElems = document.querySelectorAll('.full-elem');
+  let backBtns = document.querySelectorAll('.full-elem .back');
+  allElems.forEach(function (elem) {
+    elem.addEventListener('click', function () {
+      allFullElems[elem.id].style.display = 'block';
+    });
   });
+  backBtns.forEach(function (back) {
+    back.addEventListener('click', function () {
+      allFullElems[back.id].style.display = 'none';
+    });
+  });
+}
+openFeatures();
+let form = document.querySelector('.addTask form');
+let taskInput = document.querySelector('.addTask form input');
+let taskDetailsInput = document.querySelector('.addTask form textarea');
+let taskCheckBox = document.querySelector(' #check');
+
+form.addEventListener('submit', function (e) {
+  e.preventDefault();
+  console.log(taskInput.value);
+  console.log(taskDetailsInput.value);
+  console.log(taskCheckBox.checked);
 });
