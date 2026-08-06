d874fcf6e1111e4af75e0d77d54af45a0de5ec44
AnishBarsagade <anishbarsagade5@gmail.com>
2026-08-06 15:09:31 +0530

    Completed the motivation-page

--- productivity_dashboard/script.js
+++ productivity_dashboard/script.js
@@ -73,3 +73,56 @@ function todoList() {
   });
 }
 todoList();
+
+function dailyPlanner() {
+  let dayPlanData = JSON.parse(localStorage.getItem('dayPlanData')) || {};
+  let dayPlanner = document.querySelector('.day-planner');
+  let hours = Array.from({ length: 18 }, (_, idx) => {
+    let startHour = 6 + idx;
+    let endHour = 7 + idx;
+    let startPeriod = startHour >= 12 && startHour < 24 ? 'PM' : 'AM';
+    let endPeriod = endHour >= 12 && endHour < 24 ? 'PM' : 'AM';
+
+    let displayStart = startHour % 12 === 0 ? 12 : startHour % 12;
+    let displayEnd = endHour % 12 === 0 ? 12 : endHour % 12;
+    return `${displayStart}:00 ${startPeriod} - ${displayEnd}:00 ${endPeriod}`;
+  });
+  let sum = '';
+  hours.forEach(function (elem, idx) {
+    let savedData = dayPlanData[idx] || '';
+    sum =
+      sum +
+      `<div class="day-planner-time">
+          <p>${elem}</p>
+          <input id=${idx} type="text" placeholder=".." value=${savedData}>
+        </div>`;
+  });
+
+  dayPlanner.innerHTML = sum;
+  let dayPlannerInput = document.querySelectorAll('.day-planner input');
+
+  dayPlannerInput.forEach(function (elem) {
+    elem.addEventListener('input', function () {
+      dayPlanData[elem.id] = elem.value;
+
+      localStorage.setItem('dayPlanData', JSON.stringify(dayPlanData));
+    });
+  });
+}
+dailyPlanner();
+function motivationalQuote() {
+  let motivationQuote = document.querySelector('.motivation-2 h1');
+  let motivationAuthor = document.querySelector('.motivation-3 h2');
+  console.log(motivationAuthor);
+
+  async function fetchQuote() {
+    let response = await fetch('https://api.quotable.io/random');
+    let data = await response.json();
+    console.log(data);
+
+    motivationQuote.innerHTML = data.content;
+    motivationAuthor.innerHTML = data.author;
+  }
+  fetchQuote();
+}
+motivationalQuote();
