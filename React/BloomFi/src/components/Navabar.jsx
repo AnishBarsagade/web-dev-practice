90219aa0d30aeea00256e7b90488e319a5ef07d3
AnishBarsagade <anishbarsagade5@gmail.com>
2026-08-14 21:23:56 +0530

    Started the BloomFi project

---
+++ React/BloomFi/src/components/Navabar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import myImage from '../assets/logo.svg';
+
+const Navabar = () => {
+  return (
+    <div className="flex items-center justify-between px-8 py-4">
+      <div className="logo">
+        <img className=" h-8" src={myImage} alt="" />
+      </div>
+      <div className="nav-links flex items-center justify-between gap-4 text-sm text-black/75">
+        <a>USD Bloom</a>
+        <a>Business</a>
+        <a>Treasury</a>
+        <a>Developers</a>
+        <a>Join Us</a>
+      </div>
+      <button className="bg-black text-sm text-white px-6 py-1.5 rounded-3xl">
+        Launch Beta
+      </button>
+    </div>
+  );
+};
+
+export default Navabar;
